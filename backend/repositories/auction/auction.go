0beeb73135646279b6ff124313e5f8b320d756b4
BurningMAAN <tbumbulis@maxex.com>
2022-05-01 17:13:34 +0300

    fix

--- backend/repositories/auction/auction.go
+++ backend/repositories/auction/auction.go
@@ -104,7 +104,7 @@ func (r *repository) CreateAuction(ctx context.Context, auction models.Auction) 
 
 	_, err = r.EventWorker.PutRule(ctx, &cloudwatchevents.PutRuleInput{
 		Name:               aws.String(fmt.Sprintf("auction-event-%s", auctionID)),
-		EventPattern:       aws.String(`{"auctionID": "exampleID"}`),
+		EventPattern:       aws.String(`{"auctionID": ["exampleID"]}`),
 		ScheduleExpression: aws.String("cron(0 20 * * ? *)"),
 	})
 	if err != nil {
