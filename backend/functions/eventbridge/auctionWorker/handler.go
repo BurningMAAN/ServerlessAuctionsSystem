37932ab775b8aeb1a473ca45db81832b97b80c67
BurningMAAN <tbumbulis@maxex.com>
2022-05-01 20:44:26 +0300

    fix

--- backend/functions/eventbridge/auctionWorker/handler.go
+++ backend/functions/eventbridge/auctionWorker/handler.go
@@ -12,6 +12,7 @@ type auctionRepository interface {
 
 type eventRepository interface {
 	UpdateEventRule(ctx context.Context, auctionID string) error
+	DeleteEventRule(ctx context.Context, auctionID string) error
 }
 
 type handler struct {
@@ -40,30 +41,17 @@ func (h *handler) HandleAuction(ctx context.Context, event AuctionEvent) error {
 			log.Print(err.Error())
 			return err
 		}
+	case "STAGE_AUCTION_ONGOING":
+		err := h.auctionRepo.UpdateAuctionStage(ctx, event.AuctionID, "STAGE_AUCTION_FINISHED")
+		if err != nil {
+			log.Print(err.Error())
+			return err
+		}
 
+		err = h.eventRepository.DeleteEventRule(ctx, event.AuctionID)
+		if err != nil {
+			return err
+		}
 	}
-	// for _, eventRecord := range event.Records {
-	// 	if eventRecord.EventName == "REMOVE" {
-	// 		record := unmarshalEvent(eventRecord)
-
-	// 		switch record.Status {
-	// 		case "STATUS_ACCEPTING_BIDS":
-	// 			// Create Worker entity with startDate = null, endDate = currentTime + 33s
-	// 			endDate := time.Now().Add(33 * time.Second)
-	// 			err := h.auctionRepo.CreateAuctionWorker(ctx, record.AuctionID, "STATUS_AUCTION_ONGOING", endDate)
-	// 			if err != nil {
-	// 				log.Print(err.Error())
-	// 			}
-
-	// 		case "STATUS_AUCTION_ONGOING":
-	// 			err := h.auctionRepo.FinishAuction(ctx, record.AuctionID)
-	// 			if err != nil {
-	// 				log.Print(err.Error())
-	// 			}
-	// 		default:
-	// 			log.Printf("unsupported entity status: %s", record.Status)
-	// 		}
-	// 	}
-	// }
 	return nil
 }
--- backend/repositories/eventbridge/eventbridge.go
+++ backend/repositories/eventbridge/eventbridge.go
@@ -2,14 +2,19 @@ package eventbridge
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchevents"
+	cloudwatchTypes "github.com/aws/aws-sdk-go-v2/service/cloudwatchevents/types"
 	"github.com/aws/aws-sdk-go/aws"
 )
 
 type eventClient interface {
 	PutRule(ctx context.Context, params *cloudwatchevents.PutRuleInput, optFns ...func(*cloudwatchevents.Options)) (*cloudwatchevents.PutRuleOutput, error)
+	PutTargets(ctx context.Context, params *cloudwatchevents.PutTargetsInput, optFns ...func(*cloudwatchevents.Options)) (*cloudwatchevents.PutTargetsOutput, error)
+	DeleteRule(ctx context.Context, params *cloudwatchevents.DeleteRuleInput, optFns ...func(*cloudwatchevents.Options)) (*cloudwatchevents.DeleteRuleOutput, error)
 }
 
 type repository struct {
@@ -22,10 +27,42 @@ func New(eventClient eventClient) *repository {
 	}
 }
 
+type AuctionEvent struct {
+	AuctionID string    `json:"id"`
+	Stage     string    `json:"stage"`
+	EndDate   time.Time `json:"endDate"`
+}
+
 func (r *repository) UpdateEventRule(ctx context.Context, auctionID string) error {
 	_, err := r.eventClient.PutRule(ctx, &cloudwatchevents.PutRuleInput{
 		Name:               aws.String(fmt.Sprintf("auction-event-%s", auctionID)),
 		ScheduleExpression: aws.String("rate(5 minutes)"),
+	})
+
+	eventInput, err := json.Marshal(AuctionEvent{
+		AuctionID: auctionID,
+		Stage:     "STAGE_AUCTION_ONGOING",
+		EndDate:   time.Now().Add(33 * time.Second),
+	})
+	if err != nil {
+		return err
+	}
+	_, err = r.eventClient.PutTargets(ctx, &cloudwatchevents.PutTargetsInput{
+		Rule: aws.String(fmt.Sprintf("auction-event-%s", auctionID)),
+		Targets: []cloudwatchTypes.Target{
+			{
+				Arn:   aws.String("arn:aws:lambda:us-east-1:102336894219:function:test-backend-HandleAuctionFunction-Oa1T2FivSffq"),
+				Id:    aws.String("test-backend-HandleAuctionFunction-Oa1T2FivSffq"),
+				Input: aws.String(string(eventInput)),
+			},
+		},
+	})
+	return err
+}
+
+func (r *repository) DeleteEventRule(ctx context.Context, auctionID string) error {
+	_, err := r.eventClient.DeleteRule(ctx, &cloudwatchevents.DeleteRuleInput{
+		Name: aws.String(fmt.Sprintf("auction-event-%s", auctionID)),
 	})
 	return err
 }
