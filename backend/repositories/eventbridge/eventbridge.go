9a1281bc0e9086e0dd64be35f040c602ab7149f7
BurningMAAN <tbumbulis@maxex.com>
2022-05-01 20:54:40 +0300

    fix

--- backend/repositories/eventbridge/eventbridge.go
+++ backend/repositories/eventbridge/eventbridge.go
@@ -15,6 +15,7 @@ type eventClient interface {
 	PutRule(ctx context.Context, params *cloudwatchevents.PutRuleInput, optFns ...func(*cloudwatchevents.Options)) (*cloudwatchevents.PutRuleOutput, error)
 	PutTargets(ctx context.Context, params *cloudwatchevents.PutTargetsInput, optFns ...func(*cloudwatchevents.Options)) (*cloudwatchevents.PutTargetsOutput, error)
 	DeleteRule(ctx context.Context, params *cloudwatchevents.DeleteRuleInput, optFns ...func(*cloudwatchevents.Options)) (*cloudwatchevents.DeleteRuleOutput, error)
+	RemoveTargets(ctx context.Context, params *cloudwatchevents.RemoveTargetsInput, optFns ...func(*cloudwatchevents.Options)) (*cloudwatchevents.RemoveTargetsOutput, error)
 }
 
 type repository struct {
@@ -61,7 +62,11 @@ func (r *repository) UpdateEventRule(ctx context.Context, auctionID string) erro
 }
 
 func (r *repository) DeleteEventRule(ctx context.Context, auctionID string) error {
-	_, err := r.eventClient.DeleteRule(ctx, &cloudwatchevents.DeleteRuleInput{
+	_, err := r.eventClient.RemoveTargets(ctx, &cloudwatchevents.RemoveTargetsInput{
+		Rule: aws.String(fmt.Sprintf("auction-event-%s", auctionID)),
+		Ids:  []string{"test-backend-HandleAuctionFunction-Oa1T2FivSffq"},
+	})
+	_, err = r.eventClient.DeleteRule(ctx, &cloudwatchevents.DeleteRuleInput{
 		Name: aws.String(fmt.Sprintf("auction-event-%s", auctionID)),
 	})
 	return err
