2c0736931b1d66f394a98928a8440a59e3e2c12f
BurningMAAN <tbumbulis@maxex.com>
2022-05-14 23:45:17 +0300

    fix

--- backend/functions/api/user/updateUser/handler.go
+++ backend/functions/api/user/updateUser/handler.go
@@ -5,13 +5,14 @@ import (
 	"auctionsPlatform/utils"
 	"context"
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
 type request struct {
-	UserID        string
+	UserName      string   `json:"userId"`
 	Password      *string  `json:"password"`
 	Email         *string  `json:"email"`
 	CreditBalance *float64 `json:"creditBalance"`
@@ -26,20 +27,21 @@ type handler struct {
 }
 
 func (h *handler) UpdateUser(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
 	req := request{}
 	err := json.Unmarshal([]byte(event.Body), &req)
 	if err != nil {
 		return utils.InternalError(err.Error())
 	}
 
+	log.Printf("req body: %s", event.Body)
 	updateModel := models.UserUpdate{
-		UserName: req.UserID,
+		UserName: req.UserName,
 		Password: req.Password,
 		Email:    req.Email,
 		Credit:   req.CreditBalance,
 	}
 
+	log.Printf("after unmarshal: %v", updateModel)
 	err = h.userService.UpdateUser(ctx, updateModel)
 	if err != nil {
 		return utils.InternalError(err.Error())
